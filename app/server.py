3de8f9d72e1d04e466f2b9ba4540a0e369d93ab8
Ali Parker <aparker4416@scrippscollege.edu>
2019-02-04 23:28:35 -0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -8,7 +8,7 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=1Gbi-QX9RoS92wgvFqIye43Bd_cEO9quF'
+export_file_url = 'https://drive.google.com/uc?export=download&id=12otaFbLN-q7yznDf1kNlCEgJacK20VQ2'
 export_file_name = 'export.pkl'
 
 classes = ['black', 'grizzly', 'teddys']
