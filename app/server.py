5791337c25e8de83288c0aa2316292cb3a28e465
Dominika <33904773+dominikalad@users.noreply.github.com>
2019-02-19 11:59:06 +0100

    model changed

--- app/server.py
+++ app/server.py
@@ -8,10 +8,10 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1-7Tpl2Rkp-ky8_w5XofWh3RXWDoKV30A'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['buses', 'trains', 'trams']
 path = Path(__file__).parent
 
 app = Starlette()
