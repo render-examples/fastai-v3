85cf2b20920cf7bfde1505e1186f03ca8b359e09
zakajd <djamilzak@gmail.com>
2019-05-01 20:11:55 +0300

    updated link to the model

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from fastai import *
 from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1UONUhrKtNjtJNp5mRN-N-3bCd1Wd7-3i'
 export_file_name = 'export.pkl'
 
 classes = ['black', 'grizzly', 'teddys']
