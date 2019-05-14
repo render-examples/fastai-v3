e80ff4d8a339636e2bd1faa610ea58876bf767b8
CarlosW4E <carlorpri@gmail.com>
2019-05-14 14:51:07 +0200

    Change model data

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from fastai import *
 from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1eAbiADaxdQfT4w672j_ZwSr0_GSKyKgR'
+export_file_name = 'resnet50-9.pth'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['NOpeople', 'people']
 path = Path(__file__).parent
 
 app = Starlette()
