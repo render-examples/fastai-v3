67464e2d12336d8b69dbf93eafbf9b7743d995cb
CarlosW4E <carlorpri@gmail.com>
2019-05-14 15:15:18 +0200

    Change url data

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from fastai import *
 from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://drive.google.com/uc?export=download&id=1eAbiADaxdQfT4w672j_ZwSr0_GSKyKgR'
+export_file_url = 'https://www.dropbox.com/s/gm9blrlhilt8kv6/resnet50-9.pth?dl=1'
 export_file_name = 'resnet50-9.pth'
 
 classes = ['NOpeople', 'people']
