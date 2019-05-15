39f7bd4ba8aab6019a24909a16e9839a001ad9f6
CarlosW4E <carlorpri@gmail.com>
2019-05-15 15:02:11 +0200

    Change API

--- app/server.py
+++ app/server.py
@@ -7,6 +7,8 @@ from io import BytesIO
 
 from fastai import *
 from fastai.vision import *
+from PIL import Image
+import urllib.request
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
 export_file_url = 'https://www.dropbox.com/s/2oaahrh0nzggbkr/export.pkl?dl=1'
@@ -52,8 +54,9 @@ def index(request):
 @app.route('/analyze', methods=['POST'])
 async def analyze(request):
     data = await request.form()
-    img_bytes = await (data['file'].read())
-    img = open_image(BytesIO(img_bytes))
+    img = Image.open(urllib.request.urlopen(data['file']))
+    #img_bytes = await (data['file'].read())
+    #img = open_image(BytesIO(img_bytes))
     prediction = learn.predict(img)[0]
     return JSONResponse({'result': str(prediction)})
 
