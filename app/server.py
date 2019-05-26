11d97a13b539434eb24df0ae81c1752963761d83
davidfunberstein <33499808+davidfunberstein@users.noreply.github.com>
2019-05-27 00:02:26 +0300

    Update server.py

--- app/server.py
+++ app/server.py
@@ -10,7 +10,7 @@ from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
 export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_name = 'stage-1.pth'
 
 classes = ['black', 'grizzly', 'teddys']
 path = Path(__file__).parent
