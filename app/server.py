e3bc3afc9f1232f2e9faf8019d351cb4047981a2
davidfunberstein <33499808+davidfunberstein@users.noreply.github.com>
2019-05-27 19:59:43 +0300

    Update server.py

--- app/server.py
+++ app/server.py
@@ -65,6 +65,5 @@ async def analyze(request):
     return JSONResponse({'result': str(prediction)})
 
 
-if __name__ == '__main__':
-    if 'serve' in sys.argv:
-        uvicorn.run(app=app, host='0.0.0.0', port=5000, log_level="info")
+if __name__ == "__main__":
+    if "serve" in sys.argv: uvicorn.run(app, host="0.0.0.0", port=8080)
