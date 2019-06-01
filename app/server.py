edbe130e18c1e8be978e5ae2f3846af99cf8e237
namehtr <rtheman23@gmail.com>
2019-06-02 05:13:42 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,9 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1DEpF9-hwiZXB3DVAG17olVct1buGa8Af'
 export_file_name = 'export.pkl'
-
-classes = ['black', 'grizzly', 'teddys']
+classes = ['matar_paneer','palak_paneer','shahi_paneer','paneer_bhurji','malai_kofta','chilli_paneer','paneer_pakora']
 path = Path(__file__).parent
 
 app = Starlette()
