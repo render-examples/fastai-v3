041337f90eccd6d52d2a295247b557f2a5a01b9a
Marcello Morchio <45511108+mallibus@users.noreply.github.com>
2019-06-16 22:13:05 +0200

    Customized model and classes for learning group www.datascienceseed.com
    Robottoni classifier

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1YwyIwrpHSmicCXa2yl5fG8FpRvfbh07E'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['daitarn3', 'goldrake', 'icub', 'jeeg', 'mazinga', 'tekkaman']
 path = Path(__file__).parent
 
 app = Starlette()
