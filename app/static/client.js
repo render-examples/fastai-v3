3c846599e70c55b64e980b537c4ef216dbaa4268
Jimmy Sastra <jimmysastra@Jimmys-MacBook-Pro.local>
2019-07-04 11:48:44 -0700

    customizing text to clock

--- app/static/client.js
+++ app/static/client.js
@@ -29,7 +29,7 @@ function analyze() {
   xhr.onload = function(e) {
     if (this.readyState === 4) {
       var response = JSON.parse(e.target.responseText);
-      el("result-label").innerHTML = `Result = ${response["result"]}`;
+      el("result-label").innerHTML = `The time is ${response["result"]}:00`;
     }
     el("analyze-button").innerHTML = "Analyze";
   };
