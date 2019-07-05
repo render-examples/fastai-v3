e1bf72f3be27260ac7244a6dd5489b6f952c43b3
seanlangton01 <51829718+seanlangton01@users.noreply.github.com>
2019-07-05 14:38:43 +0200

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://www.dropbox.com/s/nwrdht9je3susi7/safetyfirst_dl_model.pkl?dl=1'
+export_file_name = 'safetyfirst_dl_model.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['cap', 'hardhat']
 path = Path(__file__).parent
 
 app = Starlette()
