cb7c5844028c38b70c5e41ced5481fec17344363
butayaro <manew93@hotmail.com>
2019-07-15 15:36:09 +0200

    Revert "change server main"
    This reverts commit 7702612a573fddf7a2701e9aba48b4f1f5059a24.

--- app/server.py
+++ app/server.py
@@ -61,12 +61,7 @@ async def analyze(request):
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
     prediction = learn.predict(img)[0]
-    if prediction =='alexandre_ferrer':
-        html_file2 = path / 'view' / 'alex.html'
-        return HTMLResponse(html_file2.open().read()) 
-    else:
-        return JSONResponse({'Personnage détecté ': str(prediction)})
-    
+    return JSONResponse({'result': str(prediction)})
 
 
 if __name__ == '__main__':
