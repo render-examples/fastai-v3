9437f1854797a6fc7e252f3a32e8a190214a3db1
jlgao2 <georgegao888@gmail.com>
2019-07-22 19:55:19 +1000

    Revert "Update server.py"
    This reverts commit 9b12b1153288e831a1647d549063be3215480e35.

--- app/server.py
+++ app/server.py
@@ -61,10 +61,7 @@ async def analyze(request):
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
     prediction = learn.predict(img)[0]
-    if prediction == 'lbj'
-        return JSONResponse({'result': 'lyndon b johnson'})
-    else 
-        return JSONResponse({'result': str(prediction)})
+    return JSONResponse({'result': str(prediction)})
 
 
 if __name__ == '__main__':
