4c5d3447bd1659b2df149ca82e8478ae7b39f3fc
humblefool02 <32545599+humblefool02@users.noreply.github.com>
2019-09-26 11:11:47 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,8 +9,8 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/open?id=1d7_3RiuZSA921X_pqLzdCUNM9WccSf_q'
+export_file_name = 'inception_transfer.h5'
 
 classes = ['black', 'grizzly', 'teddys']
 path = Path(__file__).parent
