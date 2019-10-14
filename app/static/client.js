d8c13305982b227568dd59573d24cd61e7a3006b
Deedeemo <moga.andreea87@gmail.com>
2019-10-14 21:42:36 +0200

    Update client.js

--- app/static/client.js
+++ app/static/client.js
@@ -29,7 +29,7 @@ function analyze() {
   xhr.onload = function(e) {
     if (this.readyState === 4) {
       var response = JSON.parse(e.target.responseText);
-      el("result-label").innerHTML = `Result = ${response["result"]}`;
+      el("result-label").innerHTML = ` This is a ${response["result"]}`;
     }
     el("analyze-button").innerHTML = "Analyze";
   };
