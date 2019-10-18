39c0022ae6657981b8e6738c490f96868f76b13a
HiteshAI <55893611+HiteshAI@users.noreply.github.com>
2019-10-18 12:00:39 +0545

    Update server.py

--- app/server.py
+++ app/server.py
@@ -10,7 +10,7 @@ from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
 # export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_url = 'https://drive.google.com/open?id=1YMgds-ERUKRERWXy1sNkcP-wPAT6qrDR'
+export_file_url = 'https://www.googleapis.com/drive/v3/files/1YMgds-ERUKRERWXy1sNkcP-wPAT6qrDR?alt=media&key=AIzaSyBDcJM7FN_vag_lQBanpzkFluTgauKYQMk'
 
 export_file_name = 'my_model.pkl'
 
