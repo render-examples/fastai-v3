7ddd6b25970032f3ad3b8e7672d0fef6142493bd
skydeer <58208109+skydeer@users.noreply.github.com>
2019-11-26 16:56:52 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/open?id=1aZYVVUnPl7n4am_XmTvuluCU8ik-mePZ'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['pikachu', 'squirtle', 'charmander', 'bulbasaur']
 path = Path(__file__).parent
 
 app = Starlette()
