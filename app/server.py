8c426cc8e1b8b687af7b19795ba805e04a68d5fd
Sovit Ranjan Rath <sovitrath5@gmail.com>
2019-12-19 16:13:37 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -12,7 +12,7 @@ from starlette.staticfiles import StaticFiles
 export_file_url = 'https://drive.google.com/uc?export=download&id=1cD8L4hqrjSXAgHIULsVXr_CSF_dSVewO'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['ferrari', 'lamborghini', 'tesla']
 path = Path(__file__).parent
 
 app = Starlette()
