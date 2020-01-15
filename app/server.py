274845c996b4f397ad3922abfda0ab013fd937e6
Bww38 <59789492+Bww38@users.noreply.github.com>
2020-01-15 15:29:10 +0100

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://www.googleapis.com/drive/v3/files/12Lc3iBX4OIXuUEVi6gCJR4pb6jeZybkt?alt=media&key=AIzaSyAux9Tg5ZZySR5KXYMprlrAS0Cn0_yixOU'
+export_file_name = 'export_toadstools.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['caesarea', 'crocea', 'muscaria']
 path = Path(__file__).parent
 
 app = Starlette()
