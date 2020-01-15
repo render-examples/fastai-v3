c54079e2945921a43670bd23b62a36c7271153c3
bogdanmecu <52012075+bogdanmecu@users.noreply.github.com>
2020-01-15 14:06:30 +0000

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=1mpYsX9K9OmB7bkX04HpvjibdNTDXBk_g'
+export_file_url = 'https://drive.google.com/uc?export=download&id=12jL-v6gtaSMRT_8S9GfQ-EeCuhtbmVIV'
 export_file_name = 'export.pkl'
 
 classes = ['rosemary', 'thyme', 'sage']
