2ff3eb18dfdbd75e2f14d5d3284d1f75217b7750
Neil Gordon <neil.d.gordon@gmail.com>
2020-02-01 19:35:14 +1300

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://www.dropbox.com/s/jvhjfrodorzsxq4/clouds.pkl'
+export_file_name = 'clouds.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['Altocumulus', 'Altostratus', 'Cirrocumulus', 'Cirrostratus', 'Cirrus', 'Cumulonimbus', 'Cumulus', 'Nimbostratus', 'Stratocumulus', 'Stratus']
 path = Path(__file__).parent
 
 app = Starlette()
