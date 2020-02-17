c997ea270f748d91ad1984857f0e5530418a790a
tehoro <tehoro@users.noreply.github.com>
2020-02-18 04:51:35 +1300

    Update server.py

--- app/server.py
+++ app/server.py
@@ -25,12 +25,12 @@ app.mount('/static', StaticFiles(directory='app/static'))
 
 
 async def download_file(url, dest):
-    if dest.exists(): return
+	if dest.exists(): return
 	async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            data = await response.read()
-            with open(dest, 'wb') as f:
-                f.write(data)
+		async with session.get(url) as response:
+			data = await response.read()
+			with open(dest, 'wb') as f:
+			f.write(data)
 
 
 async def setup_learner():
