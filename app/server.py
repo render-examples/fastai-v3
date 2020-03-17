75e0cf45cd089c1ae2ea4a3166fd4651df5405d2
Adam Murphy <51246969+theadammurphy@users.noreply.github.com>
2020-03-17 11:36:49 +0200

    Updated export_file_url and classes variables

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1-83N01MvQCc2UMKPk0BptuQXlxM0ofId'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['skiing', 'snowboarding', 'snowmobiling']
 path = Path(__file__).parent
 
 app = Starlette()
