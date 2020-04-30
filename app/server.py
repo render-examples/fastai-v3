a4bf2e9e7fa67bce5e282fe492cac069a2986329
Loic Chesneau <loic.chesneau@lrqdo.fr>
2020-04-30 15:10:56 +0200

    set up car model

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1r8WZ8Lhq0aL5lY_0eEy6pGkAYzqV8im-'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['4L', '2CV', 'BugattiVeyron']
 path = Path(__file__).parent
 
 app = Starlette()
