c446f87b90c06d609f423f8c272865fe2db00883
Eli Propp <59633625+Ehpropp@users.noreply.github.com>
2020-06-11 21:16:27 -0400

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,11 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1fW61igTn4zac92nR825R55hdE7pvibgO'
+export_file_name = 'Fruit_classifier.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['Apple', 'Banana', 'Blackberry', 'Blueberry', 'Lemon', 'Lime', 'Mango', 'Orange', 
+          'Pear', 'Raspberry', 'Strawberry', 'Tomato']
 path = Path(__file__).parent
 
 app = Starlette()
