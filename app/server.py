b045ec052f0a1a80e2382b5c6a4979c82f6260f2
ryuzaki-yagami <69421538+ryuzaki-yagami@users.noreply.github.com>
2020-08-13 10:54:29 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -11,7 +11,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/2w5fwbf51ovz2aj/export.pkl?dl=0'
+export_file_url = 'https://www.dropbox.com/s/2w5fwbf51ovz2aj/export.pkl?dl=1'
 export_file_name = 'export.pkl'
 
 Port = int(os.environ.get('PORT', 50000))
