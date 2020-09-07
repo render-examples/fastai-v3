7977cdb6f41f4cc67ce60ce6c7a193ba76d76a22
anderf2706 <anderf2706@gmail.com>
2020-09-07 11:53:06 +0200

    fixed to use wingr

--- app/server.py
+++ app/server.py
@@ -58,7 +58,7 @@ async def download_file(url, dest):
 
 async def setup_learner():
     try:
-        learn = load_learner(path / 'models' / 'export.pkl', export_file_name)
+        learn = load_learner(path / 'models', export_file_name)
         return learn
     except RuntimeError as e:
         if len(e.args) > 0 and 'CPU-only machine' in e.args[0]:
