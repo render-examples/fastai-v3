6854642b37b4afb5125453e465e15322de04438c
anderf2706 <anderf2706@gmail.com>
2020-11-02 18:04:36 +0100

    changing to ML_project

--- app/static/client.js
+++ app/static/client.js
@@ -30,7 +30,6 @@ function analyze() {
     if (this.readyState === 4) {
       var response = JSON.parse(e.target.responseText);
       el("result-label").innerHTML = `Result = ${response["result"]}`;
-      el("class-label").innerHTML = `Class = ${response["class"]}`;
 
     }
     el("analyze-button").innerHTML = "Analyze";
