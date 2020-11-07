3cfa83fa4cc1c1d39bf2385fe17337484d4fb90f
anderf2706 <anderf2706@gmail.com>
2020-11-07 15:49:20 +0100

    rollback

--- app/server.py
+++ app/server.py
@@ -8,6 +8,8 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
+##rollback
+
 export_file_url = 'https://drive.google.com/uc?export=download&id=1UsiVxAt91DzLt7q863Nj4vLF8nJjWwsL' #https://drive.google.com/u/0/uc?export=download&confirm=W7Y1&id=1BSva5kuYeZVnsE8M_kwO0QSILdFIgbQC
 export_file_name = 'export_ML_project.pkl'
 
