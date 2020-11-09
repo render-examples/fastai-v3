dbaa4cedefe4e5bb30d88427decbd5184f46bf56
anderf2706 <anderf2706@gmail.com>
2020-11-09 22:22:37 +0100

    rollback

--- app/server.py
+++ app/server.py
@@ -10,8 +10,8 @@ from starlette.staticfiles import StaticFiles
 
 ##rollback
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=1UsiVxAt91DzLt7q863Nj4vLF8nJjWwsL' #https://drive.google.com/u/0/uc?export=download&confirm=W7Y1&id=1BSva5kuYeZVnsE8M_kwO0QSILdFIgbQC
-export_file_name = 'export_ML_project.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1edXSYedC3AU5xdCWfaKxBCWgEec1RT9H' #https://drive.google.com/u/0/uc?export=download&confirm=W7Y1&id=1BSva5kuYeZVnsE8M_kwO0QSILdFIgbQC
+export_file_name = 'export.pkl'
 
 classes = ['NORMAL', 'PNEUMONIA']
 
@@ -49,7 +49,7 @@ path = Path(__file__).parent
 
 app = Starlette()
 app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['X-Requested-With', 'Content-Type'])
-app.mount('/static', StaticFiles(directory='app\\static'))
+app.mount('/static', StaticFiles(directory='app/static'))
 
 
 async def download_file(url, dest):
