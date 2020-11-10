e5fcc7a022de6bffeaf3191eca4f2a2b0f2a76a5
anderf2706 <anderf2706@gmail.com>
2020-11-10 11:10:56 +0100

    integrating knn

--- app/static/client.js
+++ app/static/client.js
@@ -18,7 +18,7 @@ function analyze_cnn() {
   var uploadFiles = el("file-input").files;
   if (uploadFiles.length !== 1) alert("Please select a file to analyze!");
 
-  el("analyze-button").innerHTML = "Analyzing...";
+  el("analyze_cnn-button").innerHTML = "Analyzing...";
   var xhr = new XMLHttpRequest();
   var loc = window.location;
   xhr.open("POST", `${loc.protocol}//${loc.hostname}:${loc.port}/analyze_cnn`,
@@ -32,10 +32,37 @@ function analyze_cnn() {
       el("result-label").innerHTML = `Result = ${response["result"]}`;
 
     }
-    el("analyze-button").innerHTML = "Analyze";
+    el("analyze_cnn-button").innerHTML = "Analyze";
   };
 
+
   var fileData = new FormData();
+  fileData.append("file", uploadFiles[0]);
+  xhr.send(fileData);
+}
+
+function analyze_knn() {
+  var uploadFiles = el("file-input").files;
+  if (uploadFiles.length !== 1) alert("Please select a file to analyze!");
+
+  el("analyze_knn-button").innerHTML = "Analyzing...";
+  var xhr = new XMLHttpRequest();
+  var loc = window.location;
+  xhr.open("POST", `${loc.protocol}//${loc.hostname}:${loc.port}/analyze_knn`,
+    true);
+  xhr.onerror = function() {
+    alert(xhr.responseText);
+  };
+  xhr.onload = function(e) {
+    if (this.readyState === 4) {
+      var response = JSON.parse(e.target.responseText);
+      el("result-label").innerHTML = `Result = ${response["result"]}`;
+
+    }
+    el("analyze_knn-button").innerHTML = "Analyze";
+  };
+
+    var fileData = new FormData();
   fileData.append("file", uploadFiles[0]);
   xhr.send(fileData);
 }
