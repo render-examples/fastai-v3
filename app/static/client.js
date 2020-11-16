cd6c69c541f76ba30718217e925f7a77a3f98017
Elise Raaen Bø <eliserb@dhcp-10-24-17-18.wlan.ntnu.no>
2020-11-16 19:14:20 +0100

    Made the website pretty

--- app/static/client.js
+++ app/static/client.js
@@ -14,11 +14,13 @@ function showPicked(input) {
   reader.readAsDataURL(input.files[0]);
 }
 
-function analyze() {
+function analyze() { 
   var uploadFiles = el("file-input").files;
   if (uploadFiles.length !== 1) alert("Please select a file to analyze!");
 
   el("analyze-button").innerHTML = "Analyzing...";
+
+
   var xhr = new XMLHttpRequest();
   var loc = window.location;
   xhr.open("POST", `${loc.protocol}//${loc.hostname}:${loc.port}/analyze`,
@@ -29,8 +31,13 @@ function analyze() {
   xhr.onload = function(e) {
     if (this.readyState === 4) {
       var response = JSON.parse(e.target.responseText);
+      // var response = {"result":[{'class': 'PNEUMONIA', 'output': 0.9, 'prob': 0.93}, {'class': 'NORMAL', 'output': 0.1, 'prob': 0.07}]}
       el("result-label").innerHTML = `Result = ${response["result"]}`;
-
+      var modal = document.getElementById("myModal");
+      modal.style.display = "block";
+      el("state").innerHTML = response["result"][0].class
+      // state.innerHTML = response["result"][0].class
+      el("percent").innerHTML = response["result"][0].prob * 100
     }
     el("analyze-button").innerHTML = "Analyze";
   };
@@ -39,4 +46,44 @@ function analyze() {
   fileData.append("file", uploadFiles[0]);
   xhr.send(fileData);
 }
+// Top navigation bar
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
 
+// Modal for results
+
+// When the user clicks the button, open the modal 
+// External modal-button for testing locally
+// function openModal(){
+  // var response = JSON.parse(e.target.responseText);
+  // var response = {"result":[{'class': 'PNEUMONIA', 'output': 0.9, 'prob': 0.93}, {'class': 'NORMAL', 'output': 0.1, 'prob': 0.07}]}
+  // var response = {"result":[{'class': 'NORMAL', 'output': 1.0, 'prob': 1.0}, {'class': 'PNEUMONIA', 'output': 0.0, 'prob': 0.0}]}
+  // el("result-label").innerHTML = `Result = ${response["result"]}`;
+  // var modal = document.getElementById("myModal");
+  // modal.style.display = "block";
+  // el("state").innerHTML = response["result"][0].class
+  // state.innerHTML = response["result"][0].class
+  // el("percent").innerHTML = response["result"][0].prob * 100
+// }
+
+
+// When the user clicks on <span> (x), close the modal
+function closeModal(){
+  var modal = document.getElementById("myModal");
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+
+window.onclick = function(event) {
+  var modal = document.getElementById("myModal");
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
