797f3d567f5d7dce0d1453893c0d1fc6318da4ff
anderf2706 <anderf2706@gmail.com>
2020-11-26 15:47:33 +0100

    fixes to UI, looks

--- app/server.py
+++ app/server.py
@@ -51,7 +51,7 @@ path = Path(__file__).parent
 
 app = Starlette()
 app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['X-Requested-With', 'Content-Type'])
-app.mount('/static', StaticFiles(directory='static'))
+app.mount('/static', StaticFiles(directory='app/static'))
 
 """
 async def download_file(url, dest):
