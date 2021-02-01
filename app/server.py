976f27fa7ecdce804ce1189b188c334be56e738a
jocalzaretta <71291723+jocalzaretta@users.noreply.github.com>
2021-02-01 17:18:24 -0600

    Update server.py

--- app/server.py
+++ app/server.py
@@ -13,9 +13,9 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/1yhpf0fxzwypj91/KombuchaVision_2.1.21.pkl?dl=1'
+export_file_url = 'https://www.dropbox.com/s/tjku1y86y0nsxcw/KombuchaVision_2.2.21.pkl?dl=1'
 #export_file_url = 'https://drive.google.com/uc?export=download&id=1QIbLP5c3Hwd5WyAKk8JOBUjc6mfOrYsL'
-export_file_name = 'KombuchaVision_2.1.21.pkl'
+export_file_name = 'KombuchaVision_2.2.21.pkl'
 
 classes = ['Mold', 'Not Mold']
 path = Path(__file__).parent
