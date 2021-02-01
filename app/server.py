0c964e6a3da3197f4d1bd4fd4b180d87d0650ea3
jocalzaretta <71291723+jocalzaretta@users.noreply.github.com>
2021-02-01 15:41:55 -0600

    Update server.py

--- app/server.py
+++ app/server.py
@@ -2,6 +2,7 @@ import aiohttp
 import asyncio
 import uvicorn
 from fastai import *
+from fastai.imports import *
 from fastai.vision import *
 from io import BytesIO
 from starlette.applications import Starlette
