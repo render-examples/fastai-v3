adbd8fddcac75a877dd2f24f38d58e50c5e2d988
jocalzaretta <71291723+jocalzaretta@users.noreply.github.com>
2021-02-01 19:01:09 -0600

    Update server.py

--- app/server.py
+++ app/server.py
@@ -36,9 +36,9 @@ async def download_file(url, dest):
 
 
 async def setup_learner():
-    await download_file(export_file_url, path / export_file_name)
+    await download_file(export_file_url, 'jocalzaretta/kombucha-mold-detection/app'/export_file_name)
     try:
-        learn = load_learner(path, export_file_name)
+        learn = load_learner('jocalzaretta/kombucha-mold-detection/app', export_file_name)
         return learn
     except RuntimeError as e:
         if len(e.args) > 0 and 'CPU-only machine' in e.args[0]:
