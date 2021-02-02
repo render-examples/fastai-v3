8d94e6bcf043279af1a9366d66ca1875ad58bf41
jocalzaretta <71291723+jocalzaretta@users.noreply.github.com>
2021-02-01 22:01:21 -0600

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://www.dropbox.com/s/tjku1y86y0nsxcw/KombuchaVision_2.2.21.pkl?dl=1'
 export_file_name = 'export.pkl'
 
 classes = ['black', 'grizzly', 'teddys']
