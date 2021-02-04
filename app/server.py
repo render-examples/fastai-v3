1477b05f49a29b6cc1d4aa1599d7b6b447e85fc3
jocalzaretta <71291723+jocalzaretta@users.noreply.github.com>
2021-02-04 10:38:40 -0600

    Update server.py

--- app/server.py
+++ app/server.py
@@ -45,7 +45,7 @@ async def download_file(url, dest):
 async def setup_learner():
     await download_file(export_file_url, path / export_file_name)
     try:
-        learn = load_learner(path, export_file_name)
+        learn = load_learner(path / export_file_name)
         return learn
     except RuntimeError as e:
         if len(e.args) > 0 and 'CPU-only machine' in e.args[0]:
