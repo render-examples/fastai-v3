744b5e08ac3ba1170a84b76a90d6368a7aca3ca4
jocalzaretta <71291723+jocalzaretta@users.noreply.github.com>
2021-02-04 13:06:28 -0600

    Update server.py

--- app/server.py
+++ app/server.py
@@ -89,7 +89,8 @@ async def homepage(request):
 async def analyze(request):
     img_data = await request.form()
     img_bytes = await (img_data['file'].read())
-    img = open_image(BytesIO(img_bytes))
+    #img = open_image(BytesIO(img_bytes))
+    img = PILImage.create(BytesIO(img_bytes))
     prediction = learn.predict(img)[0]
     return JSONResponse({'result': str(prediction)})
 
